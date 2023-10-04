22b66e2dc3547dcd5a33124bc22399e4e22943b7
shavy <xavier.aiguaviva@gmail.com>
2023-10-04 11:21:44 +0200

    test Chagement route deploiement

--- src/main.jsx
+++ src/main.jsx
@@ -2,35 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Outlet } from 'react-router-dom';
 import {Ressources} from './components';
 import {LegalNotice} from './components';
 import {Policy} from './components';
 
 const router = createBrowserRouter (
-  [
-   {
-    path:"/portfolio",
-     element: <App />, 
-   },
-   {
-     path:"/Ressources",
-     element: <Ressources />, 
-   },
-   {
-     path:"/Policy",
-     element: <Policy />, 
-   },
-   {
-     path:"/LegalNotice",
-     element: <LegalNotice />, 
-   },
-   {basename: "/portfolio/"}
- ]
+  createRoutesFromElements(
+    <Route exact path="/portfolio">
+      <Route exact path="/portfolio" element={<App />}/>
+      <Route exact path="/portfolio/Ressources" element={<Ressources />}/>
+      <Route exact path="/portfolio/Policy" element={<Policy />}/>
+      <Route exact path="/portfolio/LegalNotice" element={<LegalNotice />}/>
+    </Route>
+  )
+//    [
+//    {
+//     path:"/portfolio/",
+//      element: <App />, 
+//    },
+//    {
+//      path:"/portfolio/Ressources",
+//      element: <Ressources />, 
+//    },
+//    {
+//      path:"/portfolio/Policy",
+//      element: <Policy />, 
+//    },
+//    {
+//      path:"/portfolio/LegalNotice",
+//      element: <LegalNotice />, 
+//    },
+//  ]
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <RouterProvider router={router}/>
+      <Outlet/>
   </React.StrictMode>
 );
