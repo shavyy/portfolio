d49503a8ce6e54e1776fe318ed5460eceb89e0c7
shavy <xavier.aiguaviva@gmail.com>
2023-10-04 12:18:50 +0200

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2023 Xavier AIGUVAVIVA. Tous droits réservés. V1.0.2
+        Copyright Ⓒ 2023 Xavier AIGUVAVIVA. Tous droits réservés. V1.0.3
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
